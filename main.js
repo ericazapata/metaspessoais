62a56a947e82d1de2037591f00a9ab22576bf9b0
ericazapata <103613313+ericazapata@users.noreply.github.com>
2024-04-25 13:37:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-05-14T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-01T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-19T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo4 = new Date("2024-11-12T00:00:00");
+const tempoObjetivo4 = new Date("2024-11-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
@@ -41,13 +41,11 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-
     for (let i=0; i<contadores.length;i++){
-      //contadores[i].textContent = calculaTempo(tempos[i]); 
+      document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+      document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+      document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+      document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
     } 
 }
 
@@ -55,6 +53,5 @@ function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro,1000); 
 }
-
 
 comecaCronometro();
